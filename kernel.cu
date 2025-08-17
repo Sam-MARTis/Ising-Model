f7053b70e2918ab0152c74b37d9ebb5383c2c409
Sam-MARTis <samanthmartis@gmail.com>
2025-08-17 16:36:39 +0530

    Added initialization kernel and curand generator

--- kernel.cu
+++ kernel.cu
@@ -1,8 +1,11 @@
 #include "kernel.hpp"
 #include <iostream>
+#include <curand.h>
 #define TY 32
 #define TX 32
 
+#define INITIALIZATION_THRESHOLD 0.6f
+curandGenerator_t gen;
 
 __global__
 void ising_iteration(){
@@ -36,7 +39,7 @@ void convert_to_colour(bool* d_state, uchar4* d_cols, int width, int height){
     }
 }
 
-void kernelLauncher(uchar4 *d_out, float temperature, int width, int height, int iterations_per_draw){
+void IsingKernelLauncher(uchar4 *d_out, float temperature, int width, int height, int iterations_per_draw){
 
 
     const dim3 blockSize(TX, TY);
@@ -52,9 +55,49 @@ void kernelLauncher(uchar4 *d_out, float temperature, int width, int height, int
     }
     
     cudaDeviceSynchronize();
+    return;
+}
 
-    
 
 
+__global__ void initialization_kernel(uchar4 *d_out, float *d_rand, int width, int height, const float threshold) {
+    const int x = blockIdx.x * blockDim.x + threadIdx.x;
+    const int y = blockIdx.y * blockDim.y + threadIdx.y;
+    const int idx = y * width + x;
+    if (x >= width || y >= height) return;
+    if (d_rand[idx] < threshold) {
+        d_out[idx].x = 255;
+        d_out[idx].y = 0;
+        d_out[idx].z = 0;
+    } else {
+        d_out[idx].x = 0;
+        d_out[idx].y = 0;
+        d_out[idx].z = 255;
+    }
+    d_out[idx].w = 255;
+}
+
+void InitializationKernelLauncher(uchar4 *d_out, int width, int height){
+    const dim3 blockSize(TX, TY);
+    const dim3 gridSize((width + TX - 1) / TX, (height + TY - 1) / TY);
+
+    // Launch the kernel
+
+    float* d_rand;
+    cudaMalloc(&d_rand, width * height * sizeof(float));
+    curandCreateGenerator(&gen, CURAND_RNG_PSEUDO_DEFAULT);
+    curandSetPseudoRandomGeneratorSeed(gen, 1234ULL);
+
+
+    curandGenerateUniform(gen, d_rand, width * height);
+
+    initialization_kernel<<<gridSize, blockSize>>>(d_out, d_rand, width, height, INITIALIZATION_THRESHOLD);
+
+    cudaError_t err = cudaGetLastError();
+    if (err != cudaSuccess) {
+        printf("CUDA kernel launch error: %s\n", cudaGetErrorString(err));
+    }
+
+    cudaDeviceSynchronize();
     return;
 }
--- kernel.hpp
+++ kernel.hpp
@@ -2,6 +2,7 @@
 
 struct uchar4;
 
-void kernelLauncher(uchar4 *d_out, float temperature, int width, int height, int iterations_per_draw);
+void InitializationKernelLauncher(uchar4 *d_out, int width, int height);
+void IsingKernelLauncher(uchar4 *d_out, float temperature, int width, int height, int iterations_per_draw);
 
 // void testKernelLauncher(uchar4 *d_out, int width, int height);
